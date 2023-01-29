dbfa6393919e5db08382b1ebbcacec479f950b78
YeshayahuBennun <ybennun@yahoo.com>
2023-01-29 21:51:16 +0200

    Sum_Two_Linked_List - Full method

--- src/Main.java
+++ src/Main.java
@@ -10,21 +10,39 @@ public class Main {
         llB.insertNode(9);
         llB.insertNode(2);
 
-        LinkedList llC = sumLists(llA,llB);
+        LinkedList llC = sumLists(llA, llB);
 
         llC.traversalLinkedList();
-
-
     }
 
+    //  7 -> 1 -> 6 / +  / 5 -> 9 -> 2 / = /2 -> 9 -> 1/
+    //  716 +  592 = 291
     public static LinkedList sumLists(LinkedList llA, LinkedList llB) {
         LinkedList llC = new LinkedList();
 
-        llC.insertNode(7);
-        llC.insertNode(1);
-        llC.insertNode(6);
+        Node currA = llA.getHead();
+        Node currB = llB.getHead();
 
-        //TODO
+        int dec = 0;
+        int unit;
+        int aux = 0;
+
+        while (currA != null) {
+
+            aux += currA.getValue() + currB.getValue();
+            if (aux > 10) {
+                dec = aux / 10;
+                unit = aux % 10;
+                llC.insertNode(unit);
+                aux = 0;
+            } else {
+                llC.insertNode(dec + currA.getValue() + currB.getValue());
+            }
+
+            currA = currA.getNext();
+            currB = currB.getNext();
+            aux += dec;
+        }
         return llC;
     }
 }
